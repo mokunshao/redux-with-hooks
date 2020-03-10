3ce4614ec32cd32c758593ee75f4c8f4f6a560cc
mokunshao <mokunshao@foxmail.com>
2020-03-10 21:45:15 +0800

    使用 create-react-app 构建项目

---
+++ src/components/A.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const A = () => <div>A</div>;
+
+export default A;
---
+++ src/components/B.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const B = () => <div>B</div>;
+
+export default B;
